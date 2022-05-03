c6cd8895b2ad19166c991374fb453fc71fbc368f
Francine <franmelopepe2@gmail.com>
2022-05-03 15:18:46 +0200

    testimonials added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import Testimonals from './Components/Testimonals/Testimonals';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Testimonals />
     </div>
   );
 }
