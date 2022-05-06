189503f52b2330e5b54242d47bb0aa79742613ed
Francine <franmelopepe2@gmail.com>
2022-05-06 12:32:31 +0200

    grid template fixed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import './App.css';
 import FooterImages from './Components/Footer/FooterImages';
 import FooterNav from './Components/Footer/FooterNav';
-// import Footer from './Components/Footer/FooterImages';
 import Header from './Components/Header/Header';
-import MainContent from './Components/MainContent/MainContent';
 import Main from './Components/Main/Main';
 import Testimonals from './Components/Testimonals/Testimonals';
 
@@ -12,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <MainContent /> */}
       <Main />
       <Testimonals />
       <FooterImages />
